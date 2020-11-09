3394a50fbe26b95a789408c4ba2206b369af84aa
Joshua Poddoku <201751033@iiitvadodara.ac.in>
2020-11-10 03:41:21 +0530

    added a record

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          We are coming soon
+          We are coming soon! <br>hi@tupweb.tech</br>
         </p>
         <a
           className="App-link"
