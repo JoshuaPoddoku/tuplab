bb66371c8aefb18f126f02c4f442e8a9e8ffccef
Joshua Poddoku <201751033@iiitvadodara.ac.in>
2020-11-10 04:11:01 +0530

    changed hompage in pkg.json

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/company/tuplab/"
           target="_blank"
           rel="noopener noreferrer"
         >
