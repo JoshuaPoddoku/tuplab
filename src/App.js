9c8017d70102a2f93608e9810c92c3525e4d9124
Joshua Poddoku <201751033@iiitvadodara.ac.in>
2020-11-10 23:11:26 +0530

    removed br tag

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          We are coming soon! <br>hi@tupweb.tech</br>
+          We are coming soon! hi@tupweb.tech
         </p>
         <a
           className="App-link"
